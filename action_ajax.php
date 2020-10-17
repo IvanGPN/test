d5c5648ad25941b8e0fc3d0ea99a6864753b4fec
IvanGPN <gepner99@yandex.ru>
2020-10-17 13:34:04 +0300

    checking password

--- action_ajax.php
+++ action_ajax.php
@@ -27,8 +27,9 @@ $fp = fopen($file, "a");
 $log = date('Y-m-d H:i:s');
 
 if ($_POST['email'] !== '' && $_POST['name'] !== '' && $_POST['surname'] !== '' && $_POST['password'] !== '' && $_POST['repeatPass'] !== '') {
-    if ($_POST['password'] !== '' && $_POST['repeatPass'] !== '') {
-        if ($_POST['password'] == $_POST['repeatPass']) {
+
+    if ($_POST['password'] == $_POST['repeatPass']) {
+        if (strlen($_POST['password']) >= 6 && preg_match('/[A-zА-я]+/', $_POST['password']) && preg_match('/[0-9]+/', $_POST['password'])) {
             if (in_array($_POST['email'], array_column($users, 'email'))) {
                 echo json_encode(array('result' => 'Пользователь с таким email уже существует'));
                 $log = $log . ' ' . $_POST['email'] . ' found' . PHP_EOL;
@@ -39,8 +40,10 @@ if ($_POST['email'] !== '' && $_POST['name'] !== '' && $_POST['surname'] !== '' 
                 fwrite($fp, $log);
             }
         } else {
-            echo json_encode(array('result' => 'Пароли не совпадают'));
+            echo json_encode(array('result' => 'Пароль должен иметь длину более 6 символов, содержать в себе не менее 1 символа и не менее одной буквы'));
         }
+    } else {
+        echo json_encode(array('result' => 'Пароли не совпадают'));
     }
 } else {
     echo json_encode(array('result' => 'Заполните все поля'));
